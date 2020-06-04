12877baaa174f70c383a9e2dff7ed4e3be27ca84
PedroMiguel009 <zS16021250@estudiantes.uv.mx>
2020-06-04 04:48:16 -0500

    borrador 1

--- project.py
+++ project.py
@@ -104,29 +104,114 @@ def Vecinos(x):
         return [B, X, V]
 
 ######################################################################33
-Recorrido = []
-L = []
-L_i = []
-# flag = boleano
+#  Aquí se leerá el origen y destino, para luego ejecutar la función que de los valores de distancia
 
+# Para fines de evaluación del código, asigaremos Valores a las distancias
+#  Podemos calcular de una vez todas las distancias o solo ir calculando
+#  la de los vecinos para optimizar el código
+
+A.distancia=45
+B.distancia=25
+C.distancia=48
+AD.distancia=50
+HJ.distancia=39
+F.distancia=30
+H.distancia=20
+J.distancia=30
+M.distancia=47
+N.distancia=40
+O.distancia=39
+P.distancia=35
+Alv.distancia=38
+Tec.distancia=30
+Tez.distancia=18
+Sat.distancia=42
+V.distancia=21
+X.distancia=0
+Y.distancia=34
+Z.distancia=15
+
+
+###############################3#########################################
+Recorrido = []
+Lv=[]
+L=[]
+L_i = []
+L_d=[]
+
+
+print("hasta aquí vamos")
+#variables a declarar
+
+#flag = boleano
+flag=False
 
 #x= Destino   # input("nombre de la ciudad destino")
 #y= Origen   # input("nombre de la ciudad de origen")
 
-# if Destino== Origen:
-#    print("Origen y destino iguales, acabamos")
-# else:
+Destino=X
+Origen=AD
 
 
-#    Recorridox.append(x)
-#    while Recorrido[-1]=!Y:
-#        L=Vecinos(x)
-#        for i in L:
-#            flag=False
-#            for j in Recorrido:
-#                if i==j:
-#                    flag=True
-#            if flag==False:
-#                L_i.append(i)
-#        #Aquí debo ordenarlos
-#        x.hijos=L_i
+if Destino== Origen:
+    print("Origen y destino iguales, acabamos")
+
+else:
+
+
+    Recorrido.append(Origen)
+    while Recorrido[-1] != Destino:
+        Lv=Vecinos(Recorrido[-1])
+        for i in Lv:
+            flag=False
+            for j in Recorrido:
+                if i==j:
+                    flag=True
+            if flag==False:
+                #L_i.append(i)
+
+                if L_i==[]:
+                    L_i.append(i)
+                else:
+                    while flag == False:
+                        if (i.distancia >= L_i[-1].distancia):
+                            L_d.append(L_i[-1])
+                            L_i.pop(-1)
+                            if L_i == []:
+                                flag = True
+                        else:
+                            flag=True
+                    L_i.append(i)
+                    while L_d != []:
+                        L_i.append(L_d[-1])
+                        L_d.pop(-1)
+
+#                while (L_i != []) & (L_d != []):
+#                    if (L==[])or (L[-1].distancia >= L_i[-1].distancia):
+#                        L.append(L_i[-1])
+#                        L_i.pop(-1)
+#                        while L_d != []:
+#                            L.append(L_d[-1])
+#                            L_d.pop(-1)
+#                    else:
+#                        L_d.appdend(L[-1])
+#                        Lv.pop(-1)
+        Recorrido[-1].hijos = L_i
+        #print("Recorrido =",Recorrido)
+        #print("L_i =", L_i)
+        Recorrido.append(L_i[-1])
+        L_i=[]
+
+
+
+for i in Recorrido:
+    print("Recorrido en    ", i.ciudad)
+    #print(i.hijos)
+    if i.hijos != None:
+        for j in i.hijos:
+            print("Vecino:   ",j.ciudad)
+
+
+
+
+
