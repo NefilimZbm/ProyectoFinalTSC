91aa1580226546c4adfefac9a9a6b785c322d4dd
pedro <zs16021250@estudiantes.uv.mx>
2020-06-25 23:18:33 -0500

    Arreglé el imput de las ciudades

--- busquedaA*.py
+++ busquedaA*.py
@@ -2,6 +2,7 @@ from math import sin
 from math import asin
 from math import sqrt
 from math import pi
+from time import process_time
 from typing import List
 from Modules import functions as ft
 
@@ -18,32 +19,55 @@ name_city: List = ["A", "B", "C", "AD", "HJ", "F", "H", "J", "M", "N", "O", "P",
                    "Y", "Z"]
 li: List = []
 ld: List = []
-T:float
+T: float
 flag: bool = False
 origin: str = ""
 des: str = ""
 T=process_time()
 city: dict = dict(  # X = nombre [0], Vecinos [1], Coordenadas [2], Hijos [3], Distancia [4]
-    A=["Acayucan", ['M', 'Sat', 'N'], [17.94919, -94.91459], None, None],
-    B=["Boca del río", ['Alv', 'J', 'X', 'Z'], [19.10627, -96.10632], None, None],
-    C=["Coatzacoalcos", ['AD', 'M', 'Sat'], [18.13447, -94.45898], None, None],
-    AD=["Agua Dulce", ['C'], [18.14259, -94.1436], None, None],
-    HJ=["Huatla de Jiménez", ['O', 'F'], [18.13108, -96.84314], None, None],
-    F=["Fortín de las Flores", ['HJ', 'Y', 'H'], [18.9017, -96.99896], None, None],
-    H=["Huatusco", ['F', 'X'], [19.14823, -96.96654], None, None],
-    J=["Joachín", ['O', 'B', 'Y'], [18.6407, -96.23095], None, None],
-    M=["Minatitlán", ['C', 'A'], [17.99392, -94.5466], None, None],
-    N=["El Nigromante", ['A', 'O'], [17.76323, -95.75574], None, None],
-    O=["Otatitlán", ['N', 'Alv', 'J', 'HJ'], [18.18106, -96.03439], None, None],
-    P=["Papantla", ['Tec', 'V', 'Tez'], [20.45667, -97.31561], None, None],
-    Alv=["Alvarado", ['Sat', 'O', 'B'], [18.76961, -95.75894], None, None],
-    Tec=["Tecolutla", ['P', 'V'], [20.47955, -97.01012], None, None],
-    Tez=["Teziutlán", ['P', 'X'], [19.81601, -97.35705], None, None],
-    Sat=["San Andrés Tuxtla", ['C', 'A', 'Alv'], [18.44412, -95.21302], None, None],
-    V=["Vega de Alatorre", ['Tec', 'P', 'X', 'Z'], [20.03034, -96.65044], None, None],
-    X=["Xalapa", ['Tez', 'V', 'Z', 'B', 'H'], [19.54377, -96.91018], None, None],
-    Y=["Yanga", ['F', 'J'], [18.82928, -96.80027], None, None],
-    Z=["Zempoala", ['B', 'X', 'V'], [19.44688, -96.40507], None, None]
+    A=["Acayucan", ['M', 'Sat', 'N'], [17.94919, -94.91459], None, 0],
+    B=["Boca del río", ['Alv', 'J', 'X', 'Z'], [19.10627, -96.10632], None, 0],
+    C=["Coatzacoalcos", ['AD', 'M', 'Sat'], [18.13447, -94.45898], None, 0],
+    AD=["Agua Dulce", ['C'], [18.14259, -94.1436], None, 0],
+    HJ=["Huatla de Jiménez", ['O', 'F'], [18.13108, -96.84314], None, 0],
+    F=["Fortín de las Flores", ['HJ', 'Y', 'H'], [18.9017, -96.99896], None, 0],
+    H=["Huatusco", ['F', 'X'], [19.14823, -96.96654], None, 0],
+    J=["Joachín", ['O', 'B', 'Y'], [18.6407, -96.23095], None, 0],
+    M=["Minatitlán", ['C', 'A'], [17.99392, -94.5466], None, 0],
+    N=["El Nigromante", ['A', 'O'], [17.76323, -95.75574], None, 0],
+    O=["Otatitlán", ['N', 'Alv', 'J', 'HJ'], [18.18106, -96.03439], None, 0],
+    P=["Papantla", ['Tec', 'V', 'Tez'], [20.45667, -97.31561], None, 0],
+    Alv=["Alvarado", ['Sat', 'O', 'B'], [18.76961, -95.75894], None, 0],
+    Tec=["Tecolutla", ['P', 'V'], [20.47955, -97.01012], None, 0],
+    Tez=["Teziutlán", ['P', 'X'], [19.81601, -97.35705], None, 0],
+    Sat=["San Andrés Tuxtla", ['C', 'A', 'Alv'], [18.44412, -95.21302], None, 0],
+    V=["Vega de Alatorre", ['Tec', 'P', 'X', 'Z'], [20.03034, -96.65044], None, 0],
+    X=["Xalapa", ['Tez', 'V', 'Z', 'B', 'H'], [19.54377, -96.91018], None, 0],
+    Y=["Yanga", ['F', 'J'], [18.82928, -96.80027], None, 0],
+    Z=["Zempoala", ['B', 'X', 'V'], [19.44688, -96.40507], None, 0]
+)
+
+cost: dict = dict(  # [vecino, costo al vecino]
+    A=[['M', 78], ['Sat', 0], ['N', 0.0]],
+    B=[['Alv', 0], ['J', 108], ['X', 99], ['Z', 0.0]],
+    C=[['AD', 22], ['M', 0], ['Sat', 78]],
+    AD=['C', 22],
+    HJ=[['O', 0.0], ['F', 14]],
+    F=[['HJ', 14], ['Y', 0], ['H', 0]],
+    H=[['F', 0], ['X', 36]],
+    J=[['O', 0.0], ['B', 108], ['Y', 78]],
+    M=[['C', 0], ['A', 78]],
+    N=[['A', 0.0], ['O', 0.0]],
+    O=[['N', 0.0], ['Alv', 0.0], ['J', 0.0], ['HJ', 0.0]],
+    P=[['Tec', 0], ['V', 0.0], ['Tez', 0]],
+    Alv=[['Sat', 24], ['O', 0.0], ['B', 0]],
+    Tec=[['P', 0], ['V', 0.0]],
+    Tez=[['P', 0], ['X', 149]],
+    Sat=[['C', 78], ['A', 0], ['Alv', 24]],
+    V=[['Tec', 0.0], ['P', 0.0], ['X', 0.0], ['Z', 0.0]],
+    X=[['Tez', 149], ['V', 0.0], ['Z', 0.0], ['B', 99], ['H', 36]],
+    Y=[['F', 0], ['J', 78]],
+    Z=[['B', 0.0], ['X', 0.0], ['V', 0.0]]
 )
 
 while not flag:
@@ -75,9 +99,19 @@ else:
                 if i == j:
                     flag = True
             if not flag:
-                city[i][4] = ft.distance(city[i][2], city[des][2])
+                ##
+                for k in cost[i]:
+                    if k[0] == i:
+                        if k[1]==None:
+                            city[i][4] = int(ft.distance(city[i][2], city[des][2]))
+                            print("Solo distancia")
+                        else:
+                            city[i][4] = int(ft.distance(city[i][2], city[des][2])) + int(k[1])
+                            print("costo:  ",k[1])
+                #city[i][4] = ft.distance(city[i][2], city[des][2])
                 if li == []:
                     li.append(i)
+                    print("primero  ",li)
                 else:
                     while not flag:
                         if city[i][4] >= city[li[-1]][4]:
@@ -88,36 +122,15 @@ else:
                         else:
                             flag = True
                     li.append(i)
+                    print("en bucle", li)
                     while ld != []:
                         li.append(ld[-1])
                         ld.pop(-1)
         city[route[-1]][3] = li
+        print("final ",li)
         route.append(li[-1])
         li = []
 print("         Recorrido: ")
 for i in route:
     print("              ", city[i][0])
 print("Tiempo de cómputo en segundos es:    ", process_time()-T)
-
-cost: dict = dict(  # [vecino, costo al vecino]
-    A=[['M', 78], ['Sat', 0], ['N', None]],
-    B=[['Alv', 0], ['J', 108], ['X', 99], ['Z', None]],
-    C=[['AD', 22], ['M', 0], ['Sat', 78]],
-    AD=['C', 22],
-    HJ=[['O', None], ['F', 14]],
-    F=[['HJ', 14], ['Y', 0], ['H', 0]],
-    H=[['F', 0], ['X', 36]],
-    J=[['O', None], ['B', 108], ['Y', 78]],
-    M=[['C', 0], ['A', 78]],
-    N=[['A', None], ['O', None]],
-    O=[['N', None], ['Alv', None], ['J', None], ['HJ', None]],
-    P=[['Tec', 0], ['V', None], ['Tez', 0]],
-    Alv=[['Sat', 24], ['O', None], ['B', 0]],
-    Tec=[['P', 0], ['V', None]],
-    Tez=[['P', 0], ['X', 149]],
-    Sat=[['C', 78], ['A', 0], ['Alv', 24]],
-    V=[['Tec', None], ['P', None], ['X', None], ['Z', None]],
-    X=[['Tez', 149], ['V', None], ['Z', None], ['B', 99], ['H', 36]],
-    Y=[['F', 0], ['J', 78]],
-    Z=[['B', None], ['X', None], ['V', None]]
-)
